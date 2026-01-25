168d731b785ba52138dfed8be5a5238f9f7262d0
ryoha000 <the7uya@gmail.com>
2026-01-25 16:28:06 +0900

    reactnative reusablesの導入

--- client/apps/mobile/app/_layout.tsx
+++ client/apps/mobile/app/_layout.tsx
@@ -1,10 +1,13 @@
 import { Stack } from "expo-router";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { PortalHost } from "@rn-primitives/portal";
+import "../global.css"
 
 export default function RootLayout() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <Stack screenOptions={{ headerShown: false }} />
+      <PortalHost />
     </GestureHandlerRootView>
   );
 }
